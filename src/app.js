2d2631265d0b04ea63fbaebd9c3e16095d8f2e57
phuonganh1609 <164039087+phuonganh1609@users.noreply.github.com>
2026-06-05 18:21:34 +0700

    Update cors

--- src/app.js
+++ src/app.js
@@ -20,9 +20,9 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  "https://kia-car-bienhoa.vercel.app/"
   process.env.FRONTEND_URL,
 ].filter(Boolean);
+
 
 
 const corsOptions = {
